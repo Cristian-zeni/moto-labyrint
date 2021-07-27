ff674bbea44cc34a4fca73532a010abf8d10955c
cristian-zeni-marconi <cristian.zeni@marconirovereto.it>
2021-07-27 19:48:14 +0200

    sistemata posizione terreno

--- js/moto.js
+++ js/moto.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var terreno = canvas.getContext("2d");
 
 terreno.beginPath();
-terreno.rect(250, 160, 5000, 5000);
+terreno.rect(0, 0, 5000, 5000);
 terreno.fillstyle = "#FF255";
 terreno.fill();
 terreno.closePath();
