a60c22fbd3d9faa63cccdeade1762d1bc5ac4898
cristian-zeni-marconi <cristian.zeni@marconirovereto.it>
2021-08-06 15:36:26 +0200

    aggiunto cambio direzione moto

--- js/moto.js
+++ js/moto.js
@@ -7,10 +7,7 @@ var motoWidth = 10;
 var Mx = canvas.width/2;
 var My = canvas.height-30;
 
-var rightPressed = false;
-var leftPressed = false;
-var upPressed = false;
-var downPressed = false;
+var direction = 3;
 
 var dx = 5;
 var dy = 5;
@@ -21,20 +18,20 @@ document.addEventListener("keydown", keyDownHandler, false);
 
 function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
+        direction = 2;
     }
     else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
+        direction = 4;
     }
     else if(e.key == "Up" || e.key == "ArrowUp") {
-        upPressed = true;
+        direction = 1;
     }
     else if(e.key == "Down" || e.key == "ArrowDown") {
-        downPressed = true;
+        direction = 3;
     }
 }
 
-function drawCLU(){
+function drawCLU1(){
 
     terreno.beginPath();
     terreno.rect(dx, dy, motoWidth, motoHeight);
@@ -44,22 +41,38 @@ function drawCLU(){
 
 }
 
+function drawCLU2(){
+
+    terreno.beginPath();
+    terreno.rect(dx, dy, motoHeight, motoWidth);
+    terreno.fillStyle = "#FFA500";
+    terreno.fill();
+    terreno.closePath();
+
+}
+
 function draw(){
 
     terreno.clearRect(0, 0, canvas.width, canvas.height);
-    drawCLU();
     
-    if(rightPressed) {
-        dx += 2;
-    }
-    else if(leftPressed) {
-        dx -= 2;
-    }
-    else if(upPressed){
-        dy -= 2;
-    }
-    else if(downPressed){
-        dy += 2;
+    switch (direction) {
+        case 1: dy -= 2;
+                drawCLU1()
+        break;
+
+        case 2: dx += 2;
+                drawCLU2()
+        break;
+
+        case 3: dy += 2;
+                drawCLU1()
+        break;
+
+        case 4: dx -= 2;
+                drawCLU2()
+        break;
+
+        default: break;
     }
     
 }
